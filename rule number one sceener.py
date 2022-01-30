2fa90775cb0d63886f92b78a2201bf052464b3c1
Yehonatan Zion <yehonatan.zion@radcom.com>
2022-01-30 08:01:04 +0200

    [main] fix bugs and refactoring - phase 1 {2022-01-30 06:01}

--- rule number one sceener.py
+++ rule number one sceener.py
@@ -4,26 +4,26 @@ import numpy as np
 import pandas as pd
 import requests, re
 from bs4 import BeautifulSoup as bs
+import sys
+import itertools
+
 
 def Rule_num_one_ten_years_check(ticker, score):
-
     def ROIC_Check(ticker):
         score = 0
-        url = (f"https://stockanalysis.com/stocks/{ticker}/financials/ratios/")
-        res = requests.get(url)
         sum = 0.0
-        try:
-            if res.status_code == 200:
-                pass
-        except:
-            print("there was a problem downloading")
+        url = (f"https://stockanalysis.com/stocks/{ticker}/financials/ratios/")
+        res = get_request(url)
         soup = bs(res.content, 'lxml')
-        data = soup.find_all('tr')[17].text
-        tenYROIC = data[25:78].split(sep='%')#24:82
-        for i in range(len(tenYROIC)+1):
+        try:
+            data = soup.find("span", text="Return on Capital (ROIC)").parent.parent.text
+        except:
+            print("cant find ROIC selector ")
+        tenYROIC = data[24:84].split(sep='%')  # 24:82
+        for i in range(len(tenYROIC) + 1):
             sum = sum + float(tenYROIC[i])
-            if i == len(tenYROIC)-1:
-                average = sum/len(tenYROIC)
+            if i == len(tenYROIC) - 1:
+                average = sum / len(tenYROIC)
                 if average >= 9:
                     score += 1
                     print(f"{ticker} has passed ROIC check with a 10 year average ROIC of {average}")
@@ -32,31 +32,64 @@ def Rule_num_one_ten_years_check(ticker, score):
                     print(f"{ticker} didnt pass ROIC check: {average}")
                     return score
 
-
-    def Equity_growth_check(ticker):
+    def ROIC_Check_old(ticker):
         score = 0
-        url = (f"https://stockanalysis.com/stocks/{ticker}/financials/balance-sheet/")
-        res = requests.get(url)
         sum = 0.0
+        url = (f"https://stockanalysis.com/stocks/{ticker}/financials/ratios/")
+        res = get_request(url)
+        soup = bs(res.content, 'lxml')
+        data = soup.find_all('tr')[17].text
+        tenYROIC = data[25:78].split(sep='%')  # 24:82
+        for i in range(len(tenYROIC) + 1):
+            sum = sum + float(tenYROIC[i])
+            if i == len(tenYROIC) - 1:
+                average = sum / len(tenYROIC)
+                if average >= 9:
+                    score += 1
+                    print(f"{ticker} has passed ROIC check with a 10 year average ROIC of {average}")
+                    return score
+                else:
+                    print(f"{ticker} didnt pass ROIC check: {average}")
+                    return score
+
+    def get_request(url):
+        res = requests.get(url)
         try:
             if res.status_code == 200:
                 pass
         except:
-            print("there was a problem downloading")
+            print(f"there was a problem downloading {url}")
+        return res
+
+    def Equity_growth_check(ticker):
+        score = 0
+        sum = 0.0
+        url = (f"https://stockanalysis.com/stocks/{ticker}/financials/balance-sheet/")
+        res = get_request(url)
+
         soup = bs(res.content, 'lxml')
-        data = soup.find_all('tr', class_='tboldx tsep')[2]
+        try:  # TODO validate with Muli that this is the relevant value
+            # data = soup.find("span", text="Total Assets").parent.parent.text
+            table = soup.find(lambda tag: tag.name == 'table' and tag.has_attr('id') and tag['id'] == "financial-table")
+            rows = table.findAll(lambda tag: tag.name == 'tr')
+            data = [r for r in rows if r.find("span", text="Total Assets")][0]
+            data = data.findAll('td')
+        except:
+            print("cant find Total Assets")
+        # data = soup.find_all('tr', class_='border-b-2 border')[2]
         pattern = (r'''(\d+,\d*,\d*,?\d*)''')
         allYequity = re.findall(pattern, str(data))
 
-        for i in range(0, 12):
+        for i, equity in enumerate(allYequity):
             allYequity[i] = allYequity[i].replace(',', '')
             allYequity[i] = float(allYequity[i])
 
-        for i in range(0, 10):
-            growth =((allYequity[i] - allYequity[i+1])*100 / allYequity[i+1])
+        for i in range(len(allYequity) - 1):
+            growth = ((allYequity[i] - allYequity[i + 1]) * 100 / allYequity[i + 1])
             sum = sum + growth
-            if i == 9:
-                average = sum/10
+            print(f"sum: {sum}, growth: {growth}")
+            if i == len(allYequity) - 2:  # 9 TODO ask Muli what he thinks about it
+                average = sum / 9
                 if average >= 9:
                     score += 1
                     print(f"{ticker} has passed equity growth check with a 10 year average equity growth of {average}")
@@ -64,7 +97,6 @@ def Rule_num_one_ten_years_check(ticker, score):
                 else:
                     print(f"{ticker} didnt pass equity growth check: {average}")
                     return score
-
 
     def FCF_growth_Check(ticker):
         score = 0
@@ -78,12 +110,12 @@ def Rule_num_one_ten_years_check(ticker, score):
             print("there was a problem downloading")
         soup = bs(res.content, 'lxml')
         data = soup.find_all('tr', class_='tch tdent tsep')[0].text
-        tenYFCF = data[21:82].split(sep='%')#21:82
+        tenYFCF = data[21:82].split(sep='%')  # 21:82
         tenYFCF = tenYFCF[:10]
-        for i in range(len(tenYFCF)+1):
+        for i in range(len(tenYFCF) + 1):
             sum = sum + float(tenYFCF[i])
-            if i == len(tenYFCF)-1:
-                average = sum/10
+            if i == len(tenYFCF) - 1:
+                average = sum / 10
                 if average >= 9:
                     score += 1
                     print(f"{ticker} has passed Free Cash Flow Growth check with a 10 year average of {average}")
@@ -91,7 +123,6 @@ def Rule_num_one_ten_years_check(ticker, score):
                 else:
                     print(f"{ticker} didnt pass Free Cash Flow Growth check: {average}")
                     return score
-
 
     def Revenue_growth_Check(ticker):
         score = 0
@@ -107,10 +138,10 @@ def Rule_num_one_ten_years_check(ticker, score):
         data = soup.find_all('tr')[2].text
         tenYrevenue = data[14:82].split(sep='%')
         tenYrevenue = tenYrevenue[:10]
-        for i in range(len(tenYrevenue)+1):
+        for i in range(len(tenYrevenue) + 1):
             sum = sum + float(tenYrevenue[i])
-            if i == len(tenYrevenue)-1:
-                average = sum/10
+            if i == len(tenYrevenue) - 1:
+                average = sum / 10
                 if average >= 9:
                     score += 1
                     print(f"{ticker} has passed Revenue Growth check with a 10 year average of {average}")
@@ -131,12 +162,12 @@ def Rule_num_one_ten_years_check(ticker, score):
             print("there was a problem downloading")
         soup = bs(res.content, 'lxml')
         data = soup.find('tr', class_='tdent tch').text
-        tenYEPS = data[10:87].split(sep='%')#10:87
+        tenYEPS = data[10:87].split(sep='%')  # 10:87
         tenYEPS = tenYEPS[:10]
-        for i in range(len(tenYEPS)+1):
+        for i in range(len(tenYEPS) + 1):
             sum = sum + float(tenYEPS[i])
-            if i == len(tenYEPS)-1:
-                average = sum/10
+            if i == len(tenYEPS) - 1:
+                average = sum / 10
                 if average >= 9:
                     score += 1
                     print(f"{ticker} has passed EPS Growth check with a 10 year average of {average}")
@@ -144,7 +175,6 @@ def Rule_num_one_ten_years_check(ticker, score):
                 else:
                     print(f"{ticker} didnt pass EPS Growth check: {average}")
                     return score
-
 
     score1 = Equity_growth_check(ticker)
     score2 = ROIC_Check(ticker)
@@ -155,9 +185,9 @@ def Rule_num_one_ten_years_check(ticker, score):
 
     print(f'{ticker} got a score of: {score}/15 on the 10 year check.')
     return score
-    
-def get_financail_summery(stock):
 
+
+def get_financail_summery(stock):
     def get_jsonparsed_data(url):
         response = urlopen(url)
         data = response.read().decode("utf-8")
@@ -185,7 +215,8 @@ def get_financail_summery(stock):
         balance_sheet = get_jsonparsed_data(url2)
         url3 = (f"https://financialmodelingprep.com/api/v3/cash-flow-statement/{stock}?apikey={api_keys[i - 1]}")
         cash_flow = get_jsonparsed_data(url3)
-        url4 = (f"https://financialmodelingprep.com/api/v3/historical-discounted-cash-flow/{stock}?apikey={api_keys[i - 1]}")
+        url4 = (
+            f"https://financialmodelingprep.com/api/v3/historical-discounted-cash-flow/{stock}?apikey={api_keys[i - 1]}")
         DCF = get_jsonparsed_data(url4)
         url5 = (f"https://financialmodelingprep.com/api/v3/profile/{stock}?apikey={api_keys[i - 1]}")
         MC = get_jsonparsed_data(url5)
@@ -256,7 +287,7 @@ def get_financail_summery(stock):
         # year_ratios['gross_profit_growth'] = ((((((five_years_finance[(years[i])]['cost_Of_Revenue']) / (five_years_finance[(years[i])]['Revenue'])) - ((five_years_finance[(years[i + 1])]['cost_Of_Revenue']) / (five_years_finance[(years[i + 1])]['Revenue'])))) / ((five_years_finance[(years[i + 1])]['cost_Of_Revenue']) / (five_years_finance[(years[i + 1])]['Revenue']))) * 100)
         year_ratios['Net_Income_growth'] = ((((five_years_finance[(years[i])]['Net_Income']) - (
             five_years_finance[(years[i + 1])]['Net_Income'])) * 100) / (
-                                            five_years_finance[(years[i + 1])]['Net_Income']))
+                                                five_years_finance[(years[i + 1])]['Net_Income']))
         year_ratios['ROIC'] = (100 * (((five_years_finance[(years[i])]['Profit_to_work_with']) - (
             five_years_finance[(years[i])]['Dividends'])) / ((five_years_finance[(years[i])]['Liabilities']) + (
             five_years_finance[(years[i])]['Equity']))))
@@ -328,28 +359,32 @@ def get_financail_summery(stock):
                             if score >= 8:
                                 df_full.loc['Net_Income', 'Averages'] = ("score = " + str(score))
                                 df_full.to_excel(
-                                    f'C:\\Users\\noams\\Desktop\\data frames\\{stock}_financial_statement.xlsx')
+                                    f'{stock}_financial_statement.xlsx')
                                 print(f"{stock} passed rule number one test for 5 years data with the score- {score}. "
                                       "chacking 10 years data...")
                                 score = Rule_num_one_ten_years_check(stock, score)
-
+    # df_full.loc['Net_Income', 'Averages'] = ("score = " + str(score))
+    # df_full.to_excel(f'score_{score}_{stock}_financial_statement.xlsx')
     print(f"{stock} got a score of {score}")
     if score == 7:
         answer = input("do you still want to make an excel file?")
         if answer == 'yes':
             df_full.loc['Net_Income', 'Averages'] = ("score = " + str(score))
-            df_full.to_excel(f'C:\\Users\\noams\\Desktop\\data frames\\{stock}_financial_statement.xlsx')
+            df_full.to_excel(f'{stock}_financial_statement.xlsx')
     # answer = input("do you want to check another stock? ")
     # if answer == 'yes' or ' ':
     #     get_financail_summery()
 
     # return print(f"{stock} got a score of {score}")
 
-if len(sys.argv) > 1:
-    stock = sys.argv[1]
-else:
-    stock = input("Please enter a symbol for a 'rule number one' test: ").upper()
-   
-get_financail_summery(stock)
-   
+
+if __name__ == "__main__":
+    print("hi")
+    if len(sys.argv) > 1:
+        stock = sys.argv[1]
+    else:
+        stock = input("Please enter a symbol for a 'rule number one' test: ").upper()
+
+    get_financail_summery(stock)
+
 
